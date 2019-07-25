126260a4440005b482bd2bcf18439bdc2ae239b6
Snehal Sadashiv Bodke (Digital) <SN20098787@wipro.com>
2019-07-25 11:41:55 +0530

    assignment for cloudNativeDeveloperDatabaseAccess.

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -3,14 +3,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
-    @Bean
+    /*@Bean
     TimeEntryRepository timeEntryRepository() {
         return new InMemoryTimeEntryRepository();
+    }*/
+
+    @Bean
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
